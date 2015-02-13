810de3c2b135cb4cd07ab96dc9091f424ec925df
Aaron Krause <aaronjkrause@gmail.com>
2015-02-13 00:01:14 -0800

    tests work

--- test/rest-test.js
+++ test/rest-test.js
@@ -8,14 +8,14 @@ var fs = require('fs');
 
 describe('REST tests', function(){
   var server = 'localhost:3000';
-  before(function(){
+  var count = fs.readdirSync('./data').length + 1;
+  beforeEach(function(){
     var file = JSON.stringify({firstName:'Randy', lastName:'Savage', snapsInto:'Slim Jim'});
-    count = fs.readdirSync('./data').length;
     fs.writeFileSync('./data/testFile.json', file, 'utf-8');
-
   })
   after(function(){
     fs.unlinkSync('./data/testFile.json');
+    fs.unlinkSync('./data/' + count + '.json')
   })
   it('should get a directory for GET', function(done){
     chai.request(server)
@@ -39,14 +39,35 @@ describe('REST tests', function(){
   })
   it('should modify test file for PATCH', function(done){
     chai.request(server)
-      .put('/user/testFile')
+      .patch('/user/testFile')
       .send({oh:'YEAH'})
       .end(function(err, res){
         expect(err).to.eql(null);
         expect(res).to.have.status(200);
-        expect(res.text).to.contain('YEAH');
-        expect(fs.readFileSync('./data/testFile.json', 'utf8')).to.contain('Randy')
+        expect(fs.readFileSync('./data/testFile.json', 'utf8')).to.contain('YEAH')
         done();
       });
+  })
+  it('should replace test file for PUT', function(done){
+    chai.request(server)
+      .put('/user/testFile')
+      .send({firstName:'Hulk', lastName:'Hogan', hates:'machomanrandysavage'})
+      .end(function(err, res){
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(fs.readFileSync('./data/testFile.json', 'utf8')).to.contain('machomanrandysavage');
+        done();
+      })
+  })
+  it('should create a new file for POST', function(done){
+    chai.request(server)
+      .post('/user')
+      .send({firstName:'Iron', lastName:'Sheik', move:'Camel Clutch'})
+      .end(function(err, res){
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(fs.readFileSync('./data/' + count + '.json', 'utf8')).to.contain('Camel Clutch')
+        done();
+      })
   })
 })
